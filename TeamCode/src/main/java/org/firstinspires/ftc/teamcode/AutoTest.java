0e0fb0ab91b1820e1ebe35fb45dcb65ac751fef1
TT Computer <technotumbleweeds@gmail.com>
2024-12-05 19:02:01 -0800

    Started New Auto - Hunter

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class AutoTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws  InterruptedException {
+
+        //https://www.youtube.com/watch?v=X8ZsJ6pl3XI
+
+    }
+}
+
