a9ccd6b4bf064651c7bfd24ae71e7f33326dd2f1
Techno-Tumbleweeds-Landen <Landenheine17@gmail.com>
2024-12-19 20:17:36 -0600

    Added new positions and logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,30 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
-
-
-
+//imports packages
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
-
-
-
-
-
-
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp")
 public class TeleOp extends OpMode {
-
-
-
-
-
-
-
-
+    //Declares names of components
     DcMotor FrontLeft;
     DcMotor FrontRight;
     DcMotor BackLeft;
@@ -34,51 +19,18 @@ public class TeleOp extends OpMode {
     Servo ArmJoint;
     Servo claw;
 
-
-
-
-
-
-
-
+    //Sets variables
     double armPos =0;
-
-
-
-
-
-
-
-
+    double armPosfreeze;
+    boolean armFree = false;
     double motorSpeed = 0.8;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     //boolean isMoving = false;
     //double armPosfreeze = 0.35;
 
 
-
-
-
-
-
-
     @Override
     public void init() {
+        //finds components in robot configuration (Motors)
         FrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
         FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
         BackLeft = hardwareMap.get(DcMotor.class,"BackLeft");
@@ -86,183 +38,44 @@ public class TeleOp extends OpMode {
         ArmMotorR = hardwareMap.get(DcMotor.class, "ArmMotorR");
         ArmMotorL = hardwareMap.get(DcMotor.class, "ArmMotorL");
 
-
-
-
-
-
-
-
-
-
+        //finds components in robot configuration (Servos)
         claw = hardwareMap.get(Servo.class, "claw");
-        ArmJoint = hardwareMap.get(Servo.class, "ArmJoint"); // Initialize the Servo
+        ArmJoint = hardwareMap.get(Servo.class, "ArmJoint");
 
-
-
-
-
-
-
-
+        //sets directions of movement motors
         FrontLeft.setDirection(DcMotor.Direction.FORWARD);
         FrontRight.setDirection(DcMotor.Direction.REVERSE);
         BackLeft.setDirection(DcMotor.Direction.FORWARD);
         BackRight.setDirection(DcMotor.Direction.REVERSE);
 
-
-
-
-
-
-
-
+        //sets directions of arm motors
         ArmMotorR.setDirection(DcMotorSimple.Direction.REVERSE);
         ArmMotorL.setDirection(DcMotorSimple.Direction.REVERSE);
 
-
-
-
-        //Slows drop of lift kit
+        //slows drop of lift kit
         ArmMotorR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         ArmMotorL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-
-
-
+        //resets encoders in each motor in the lift kit
         ArmMotorR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         ArmMotorL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-
-
-
+        //prints on the drivers hub "Initialization: Initialized"
         telemetry.addData("Initialization:", "Initialized");
         telemetry.update();
     }
 
-
-
-
-
-
-
-
     @Override
+    //main loop
     public void loop() {
+        //powers drivetrain
         FrontRight.setPower((((gamepad1.right_trigger - gamepad1.left_trigger - gamepad1.left_stick_x)) - (gamepad1.right_stick_x * 0.85)) * motorSpeed);
         FrontLeft.setPower((((gamepad1.right_trigger - gamepad1.left_trigger + gamepad1.left_stick_x)) + (gamepad1.right_stick_x * 0.85)) * motorSpeed);
         BackLeft.setPower((((gamepad1.right_trigger - gamepad1.left_trigger + gamepad1.left_stick_x)) - (gamepad1.right_stick_x)) * motorSpeed);
         BackRight.setPower((((gamepad1.right_trigger - gamepad1.left_trigger - gamepad1.left_stick_x)) + (gamepad1.right_stick_x)) * motorSpeed);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //top basket
-        if (gamepad2.dpad_up) {
-            //sets position for arm height an moves it there
-            //ArmMotor.setTargetPosition(2750);
-            //ArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            //ArmMotor.setPower(0.7);
-
-
-
-
-
-
-
-
-            armPos = 0.5;
-        }
-
-
-
-
-
-
-
-
-        //top rung
-       /*if (gamepad2.dpad_left) {
-           //sets position for arm height an moves it there
-           //ArmMotor.setTargetPosition(1200);
-           //ArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-           //ArmMotor.setPower(0.6);
-
-
-
-
-
-
-
-
-       }
-
-
-        */
-
-
-
-
-
-
-
-
-        //bottom
-        if (gamepad2.dpad_down) {
-            //sets position for arm height an moves it there
-            //ArmMotor.setTargetPosition(275);
-            //ArmMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            //ArmMotor.setPower(0.25);
-
-
-
-
-            armPos = 0.146;
-        }
-
-
-
-
-        if (gamepad2.back){
-
-            armPos = 0.95;
-        }
-
-
-
-     /*
-     if (gamepad2.triangle) {
-         armPos = 0.65;
-         armPosfreeze = armPos;
-     } else if (gamepad2.square) {
-         armPos = 0.45;
-         armPosfreeze = armPos;
-     } else if (gamepad2.cross) {
-         armPos = 0.36;
-         armPosfreeze = armPos;
-     } else if (gamepad2.left_bumper) {
-         armPos = 0.31;
-     } else if (!gamepad2.left_bumper) {
-         armPos = armPosfreeze;
-     }
-      */
-
-
-
-
-        if(gamepad1.triangle){
+        //sets speed of motors
+        if (gamepad1.triangle) {
             motorSpeed = 0.8;
         } else if (gamepad1.square) {
             motorSpeed = 0.5;
@@ -270,7 +83,44 @@ public class TeleOp extends OpMode {
             motorSpeed = 0.275;
         }
 
+        //detects button press to set servo mode to free
+        if (gamepad2.dpad_left){
+            armFree = !armFree;
+        }
 
+        //arm free
+        if (armFree){
+            armPos = -((gamepad2.left_stick_y)/2 - 0.5);
+        //arm up position
+        }else if (gamepad2.dpad_up){
+            armPos = 0.5;
+            armPosfreeze = armPos;
+        //arm down position
+        } else if (gamepad2.dpad_down){
+            armPos = 0.146;
+            armPosfreeze = armPos;
+        //arm start position
+        } else if (gamepad2.back){
+            armPos = 0.95;
+            armPosfreeze = armPos;
+        //arm grabbing position
+        } else if (gamepad2.left_bumper){
+            armPos = 0.05;
+        //arm back to last position before bumper
+        } else if (!gamepad2.left_bumper){
+            armPos = armPosfreeze;
+        }
+
+        //moves servo
+        ArmJoint.setPosition(armPos);
+
+        //closes or opens claw
+        if (gamepad2.right_bumper) {
+            claw.setPosition(0.8);
+        }
+        else{
+            claw.setPosition(0.5);
+        }
 
 
         //manual control
@@ -288,35 +138,6 @@ public class TeleOp extends OpMode {
             // Set motors back to RUN_USING_ENCODER mode for other operations
             ArmMotorL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
-
-
-
-
-
-
-
-
-    armPos = (gamepad2.left_stick_y)/2 + 0.5;
-
-
-
-     // ArmJoint.setPosition(armPos + (0 - gamepad2.left_stick_y));
-        ArmJoint.setPosition(armPos);
-
-
-
-
-
-
-        if (gamepad2.right_bumper){
-            claw.setPosition(0.8);
-        }
-        else{
-            claw.setPosition(0.5);
-        }
-
-
-
 
         // Example telemetry for servo position
         telemetry.addData("Servo Position", ArmJoint.getPosition());
