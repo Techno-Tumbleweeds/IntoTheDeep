89c0bec3c00519c113ff8bb6b5bcab8f92a25d9b
Techno-Tumbleweeds-Landen <Landenheine17@gmail.com>
2025-01-08 18:34:00 -0600

    Working on huskey lens

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HunterAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HunterAuto.java
@@ -1,12 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "AutoHunter", group = "Auto")
+@Autonomous(name = "AutoHunter", group = "Auto")
 public class HunterAuto extends LinearOpMode {
 
     // Declare hardware components
