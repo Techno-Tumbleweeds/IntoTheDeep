c87811c2d167db246342a23d0b1e77bfaeb1f69f
Techno-Tumbleweeds-Landen <Landenheine17@gmail.com>
2025-01-09 18:49:14 -0600

    Got code figured out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoHuskeyLens.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoHuskeyLens.java
@@ -21,13 +21,70 @@ public class AutoHuskeyLens extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
 
-    static final double     COUNTS_PER_MOTOR_REV    = 537.7 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
-    static final double     WHEEL_DIAMETER_INCHES   = 4.09449 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+    static final double COUNTS_PER_MOTOR_REV = 537.7;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // No External Gearing.
+    static final double WHEEL_DIAMETER_INCHES = 4.09449;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.6;
-    static final double     TURN_SPEED              = 0.5;
+    static final double DRIVE_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
+
+    public void encoderDrive(double speed,
+                                double leftInches, double rightInches,
+                                double timeoutS, long waitTime) {
+        int newLeftTarget;
+        int newRightTarget;
+
+
+        // Ensure that the OpMode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = FrontLeft.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRightTarget = FrontRight.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            FrontLeft.setTargetPosition(newLeftTarget);
+            FrontRight.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            FrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            FrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            FrontLeft.setPower(Math.abs(speed));
+            FrontRight.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (FrontLeft.isBusy() && FrontRight.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Running to", " %7d :%7d", newLeftTarget, newRightTarget);
+                telemetry.addData("Currently at", " at %7d :%7d",
+                        FrontLeft.getCurrentPosition(), FrontRight.getCurrentPosition());
+                telemetry.update();
+
+            }
+
+
+        }
+    }
+
+    private void stopMotors() {
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+    }
+
+    private void moveForward(double power) {
+        FrontLeft.setPower(power);
+        FrontRight.setPower(power);
+    }
 
     @Override
     public void runOpMode() {
@@ -46,7 +103,7 @@ public class AutoHuskeyLens extends LinearOpMode {
         FrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Starting at",  "%7d :%7d",
+        telemetry.addData("Starting at", "%7d :%7d",
                 FrontLeft.getCurrentPosition(),
                 FrontRight.getCurrentPosition());
         telemetry.update();
@@ -68,6 +125,9 @@ public class AutoHuskeyLens extends LinearOpMode {
         telemetry.update();
         waitForStart();
 
+        encoderDrive(DRIVE_SPEED,  -52,  -52, 10, 225);  // S1: Forward 47 Inches with 5 Sec timeout
+
+
         while (opModeIsActive()) {
             if (!rateLimit.hasExpired()) {
                 continue;
@@ -77,7 +137,7 @@ public class AutoHuskeyLens extends LinearOpMode {
             HuskyLens.Block[] blocks = Cam.blocks();
             telemetry.addData("Block count", blocks.length);
 
-            /*
+
             for (HuskyLens.Block block : blocks) {
                 telemetry.addData("Block", block.toString());
 
@@ -90,76 +150,8 @@ public class AutoHuskeyLens extends LinearOpMode {
                 }
             }
 
-             */
-
 
             telemetry.update();
         }
-
-        public void encoderDrive(double speed,
-                double leftInches, double rightInches,
-                double timeoutS, long waitTime) {
-            int newLeftTarget;
-            int newRightTarget;
-
-
-            // Ensure that the OpMode is still active
-            if (opModeIsActive()) {
-
-                // Determine new target position, and pass to motor controller
-                newLeftTarget = FrontLeft.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
-                newRightTarget = FrontRight.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
-                FrontLeft.setTargetPosition(newLeftTarget);
-                FrontRight.setTargetPosition(newRightTarget);
-
-                // Turn On RUN_TO_POSITION
-                FrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                FrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-                // reset the timeout time and start motion.
-                runtime.reset();
-                FrontLeft.setPower(Math.abs(speed));
-                FrontRight.setPower(Math.abs(speed));
-
-                // keep looping while we are still active, and there is time left, and both motors are running.
-                // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-                // its target position, the motion will stop.  This is "safer" in the event that the robot will
-                // always end the motion as soon as possible.
-                // However, if you require that BOTH motors have finished their moves before the robot continues
-                // onto the next step, use (isBusy() || isBusy()) in the loop test.
-                while (opModeIsActive() &&
-                        (runtime.seconds() < timeoutS) &&
-                        (FrontLeft.isBusy() && FrontRight.isBusy())) {
-
-                    // Display it for the driver.
-                    telemetry.addData("Running to",  " %7d :%7d", newLeftTarget,  newRightTarget);
-                    telemetry.addData("Currently at",  " at %7d :%7d",
-                            FrontLeft.getCurrentPosition(), FrontRight.getCurrentPosition());
-                    telemetry.update();
-
-                }
-
-                // Stop all motion;
-                FrontLeft.setPower(0);
-                FrontRight.setPower(0);
-
-                // Turn off RUN_TO_POSITION
-                FrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                FrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-                sleep(waitTime);   // optional pause after each move.
-            }
     }
-
 }
-
-        private void stopMotors() {
-            FrontLeft.setPower(0);
-            FrontRight.setPower(0);
-        }
-
-        private void moveForward(double power) {
-            FrontLeft.setPower(power);
-            FrontRight.setPower(power);
-        }
-    }
